71c54ff9401e4da4c955dfe4c78d5ba24bba36ae
FatherFoxxy <chris.altig@gmail.com>
2015-09-09 20:41:59 -0700

    Lots of changes made to Configuration. Expand the commit to view. CHANGELOG: -Renamed Config.cs to ConfigHandler.cs -Renamed RootConfig.cs to Config.cs -Changed auto-generated config to generate blank fields rather than null ones -Deleted useless vestigial classes pulled from a separate bot -ConfigHandler.cs now no longer contains an instance of Config.cs but instead returns a Config object when calling Reload() -Changed IConfig.cs to reflect changes made to ConfigHandler.cs -Removed useless variables in ConfigHandler.cs such as WebTradeEligiblity.

---
+++ CSharpTradeOffers/Configuration/ConfigHandler.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CSharpTradeOffers.Configuration
+{
+    /// <summary>
+    /// Generic config file handler
+    /// </summary>
+    public class ConfigHandler : IConfig
+    {
+        private readonly string _path;
+
+        /// <summary>
+        /// Initializes the Config and the path to use
+        /// </summary>
+        /// <param name="path"></param>
+        public ConfigHandler(string path)
+        {
+            _path = path;
+        }
+
+        /// <summary>
+        /// Reloads the configuration file (path). If file is not present, it will generate a new one.
+        /// </summary>
+        /// <returns>A RootConfig object.</returns>
+        public Config Reload()
+        {
+            var config = new Config();
+
+            if (!File.Exists(_path))
+            {
+                File.Create(_path).Close();
+
+                #region append
+
+                // BuildMyString.com generated code. Please enjoy your string responsibly.
+
+                var sb = new StringBuilder();
+
+                sb.Append("{\r\n");
+                sb.Append("    \"Username\": \"\",\r\n");
+                sb.Append("    \"Password\": \"\",\r\n");
+                sb.Append("    \"ApiKey\": \"\",\r\n");
+                sb.Append("    \"SteamMachineAuth\": \"\",\r\n");
+                sb.Append("    \"Inventories\":[440,730]\r\n");
+                sb.Append("}\r\n");
+
+                #endregion
+
+                File.WriteAllText(_path, sb.ToString());
+            }
+
+            config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(_path));
+
+            return config;
+        }
+
+        /// <summary>
+        /// Writes the changes made to the config.
+        /// </summary>
+        /// <param name="towrite"></param>
+        public void WriteChanges(Config towrite)
+        {
+            File.WriteAllText(_path, JsonConvert.SerializeObject(towrite));
+        }
+    }
+}
--- CSharpTradeOffers/Configuration/IConfig.cs
+++ CSharpTradeOffers/Configuration/IConfig.cs
@@ -1,8 +1,12 @@
 ﻿namespace CSharpTradeOffers.Configuration
 {
+    /// <summary>
+    /// IConfig interface so the default ConfigHandler that
+    /// uses JSON does not have to be used.
+    /// </summary>
     public interface IConfig
     {
-        RootConfig Reload();
-        void WriteChanges(RootConfig toWrite);
+        Config Reload();
+        void WriteChanges(Config toWrite);
     }
 }
