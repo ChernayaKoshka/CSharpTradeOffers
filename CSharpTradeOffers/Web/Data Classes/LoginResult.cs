c4173297884bf7364f64c1e1be89c2f9dceb2822
FatherFoxxy <chris.altig@gmail.com>
2015-09-16 23:01:32 -0700

    Moved items from Json folder to Web.DataClasses folder

---
+++ CSharpTradeOffers/Web/Data Classes/LoginResult.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace CSharpTradeOffers.Web
+{
+    [JsonObject(Title = "RootObject")]
+    public class LoginResult
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+        [JsonProperty("requires_twofactor")]
+        public bool RequiresTwofactor { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("captcha_needed")]
+        public bool CaptchaNeeded { get; set; }
+        [JsonProperty("captcha_gid")]
+        public string CaptchaGid { get; set; }
+        [JsonProperty("emailauth_needed")]
+        public bool EmailAuthNeeded { get; set; }
+        [JsonProperty("emaildomain")]
+        public string EmailDomain { get; set; }
+        [JsonProperty("emailsteamid")]
+        public string EmailSteamId { get; set; }
+        [JsonProperty("login_complete")]
+        public bool LoginComplete { get; set; }
+        [JsonProperty("transfer_url")]
+        public string TransferUrl { get; set; }
+        [JsonProperty("transfer_parameters")]
+        public TransferParameters TransferParameters { get; set; }
+    }
+}
---
+++ CSharpTradeOffers/Web/Data Classes/RsaKey.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace CSharpTradeOffers.Web
+{
+    public class RsaKey
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+        [JsonProperty("publickey_mod")]
+        public string PublicKeyMod { get; set; }
+        [JsonProperty("publickey_exp")]
+        public string PublicKeyExp { get; set; }
+        [JsonProperty("timestamp")]
+        public string TimeStamp { get; set; }
+    }
+
+}
