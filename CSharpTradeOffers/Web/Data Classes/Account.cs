aadd20bba935d672cd5645301118ecc63e697779
FatherFoxxy <chris.altig@gmail.com>
2015-11-07 22:03:37 -0800

    Added some validation of SteamId, added SteamMachineAuth string variable to replace SteamMachineAuth in Web.cs

--- CSharpTradeOffers/Web/Data Classes/Account.cs
+++ CSharpTradeOffers/Web/Data Classes/Account.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace CSharpTradeOffers.Web
 {
@@ -10,6 +11,8 @@ namespace CSharpTradeOffers.Web
         public Account(ulong steamId)
         {
             //add validation later
+            if (steamId < 76561197960265728)
+                throw new ArgumentException("Invalid SteamId64, must be greater than 76561197960265728!");
             SteamId = steamId;
         }
 
@@ -23,15 +26,26 @@ namespace CSharpTradeOffers.Web
         /// </summary>
         public readonly CookieContainer AuthContainer = new CookieContainer();
 
+        private string _steamMachineAuth;
+
+        /// <summary>
+        /// Sets the SteamMachineAuth string as well as adding the auth string to the AuthContainer.
+        /// </summary>
+        public string SteamMachineAuth
+        {
+            get { return _steamMachineAuth; }
+            set { _steamMachineAuth = value; AddMachineAuthCookies(value); }
+        }
+
         /// <summary>
         /// Adds the steamMachineAuth cookie to the AuthContainer.
         /// This can also be used for any string separated by [name]=[value]
         /// </summary>
         /// <param name="authstring">String to turn into a cookie and add.</param>
-        public void AddMachineAuthCookies(string authstring)
+        private void AddMachineAuthCookies(string authstring)
         {
             string[] split = authstring.Split('=');
-            AuthContainer.Add(new Cookie(split[0], split[1]) {Domain = "steamcommunity.com"});
+            AuthContainer.Add(new Cookie(split[0], split[1]) { Domain = "steamcommunity.com" });
         }
     }
 }
