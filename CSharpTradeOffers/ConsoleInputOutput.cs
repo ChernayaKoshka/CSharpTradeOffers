ef928bb6d5017c47d251f50344c8029d02c715cd
FatherFoxxy <chris.altig@gmail.com>
2015-12-14 16:50:08 -0800

    Added ConsoleInputOutput which implements IUserInputOutput. This is the default UserInputOutput for every class that uses it

---
+++ CSharpTradeOffers/ConsoleInputOutput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpTradeOffers
+{
+    public class ConsoleInputOutput : IUserInputOutputHandler
+    {
+        public string GetInput()
+        {
+            return Console.ReadLine();
+        }
+
+        public void OutputMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
