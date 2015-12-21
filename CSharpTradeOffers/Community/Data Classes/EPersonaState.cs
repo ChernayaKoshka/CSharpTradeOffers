7357d90730740585bbe9bd378544ec75f06a21d3
FatherFoxxy <chris.altig@gmail.com>
2015-12-20 21:08:20 -0800

    Added enum PersonaState and implemented into FriendStateResponse as well as adding other properties to FriendStateResponse like "InGameName"

---
+++ CSharpTradeOffers/Community/Data Classes/EPersonaState.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpTradeOffers.Community
+{
+    public enum EPersonaState
+    {
+        Offline = 0,
+        Online = 1,
+        Busy = 2,
+        Away = 3,
+        Snooze = 4
+    }
+}
--- CSharpTradeOffers/Community/Data Classes/FriendStateResponse.cs
+++ CSharpTradeOffers/Community/Data Classes/FriendStateResponse.cs
@@ -6,21 +6,37 @@ namespace CSharpTradeOffers.Community
     public class FriendStateResponse
     {
         [JsonProperty("m_unAccountID")]
-        public int AccountID { get; set; }
+        public int AccountId { get; set; }
+
         [JsonProperty("m_ulSteamID")]
-        public string SteamID { get; set; }
+        public string SteamId { get; set; }
+
         [JsonProperty("m_strName")]
         public string Name { get; set; }
+
         [JsonProperty("m_ePersonaState")]
-        public int PersonaState { get; set; }
+        public EPersonaState PersonaState { get; set; }
+
         [JsonProperty("m_nPersonaStateFlags")]
         public object PersonaStateFlags { get; set; }
+
         [JsonProperty("m_strAvatarHash")]
         public string AvatarHash { get; set; }
+
         [JsonProperty("m_tsLastMessage")]
         public int LastMessage { get; set; }
+
         [JsonProperty("m_tsLastView")]
         public int LastView { get; set; }
+
+        [JsonProperty("m_bInGame")]
+        public bool InGame { get; set; }
+
+        [JsonProperty("m_nInGameAppID")]
+        public string InGameAppId { get; set; }
+
+        [JsonProperty("m_strInGameName")]
+        public string InGameName { get; set; }
     }
 
 }
