cdbdf397477bfc1e4037d2a3a34c41a0e64ee2ac
FatherFoxxy <chris.altig@gmail.com>
2015-12-22 10:56:28 -0800

    Added ability to set your own loading screen text

--- samples/SteamWebChat/LoadingScreen.xaml.cs
+++ samples/SteamWebChat/LoadingScreen.xaml.cs
@@ -1,13 +1,29 @@
-﻿namespace SteamWebChat
+﻿using System.Windows;
+
+namespace SteamWebChat
 {
     /// <summary>
     /// Interaction logic for LoadingScreen.xaml
     /// </summary>
     public partial class LoadingScreen
     {
+        private readonly string _text;
+
         public LoadingScreen()
         {
             InitializeComponent();
+        }
+
+        public LoadingScreen(string text)
+        {
+            _text = text;
+            InitializeComponent();
+            Loaded += Form_Loaded;
+        }
+
+        void Form_Loaded(object sender, RoutedEventArgs e)
+        {
+            statusLabel.Content = _text;
         }
     }
 }
