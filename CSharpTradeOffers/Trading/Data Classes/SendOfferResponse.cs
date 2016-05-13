7d9dc33e29b7d55307c3376aef1db396d5a3941e
Christopher A <chris.altig@gmail.com>
2016-05-12 18:08:51 -0700

    Added Error and NeedsMobileConfirmation fields

--- CSharpTradeOffers/Trading/Data Classes/SendOfferResponse.cs
+++ CSharpTradeOffers/Trading/Data Classes/SendOfferResponse.cs
@@ -11,7 +11,13 @@ namespace CSharpTradeOffers.Trading
         [JsonProperty("needs_email_confirmation")]
         public bool NeedsEmailConfirmation { get; set; }
 
+        [JsonProperty("needs_mobile_confirmation")]
+        public bool NeedsMobileConfirmation { get; set; }
+
         [JsonProperty("email_domain")]
         public string EmailDomain { get; set; }
+
+        [JsonProperty("strError")]
+        public string Error { get; set; }
     }
 }
